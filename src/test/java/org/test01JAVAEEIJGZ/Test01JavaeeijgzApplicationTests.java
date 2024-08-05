7cc2929ac560435353ad1351c47600476328f2e1
IGabriel30 <ingridgabriel30@hotmail.com>
2024-08-05 12:24:52 -0600

    se inicializa y se estructura el proyecto

---
+++ src/test/java/org/test01JAVAEEIJGZ/Test01JavaeeijgzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test01JAVAEEIJGZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test01JavaeeijgzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
